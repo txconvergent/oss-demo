3df8bbad3afb3727e275936b39994cdbb0e4c55c
Hassaan Raza <RAZA@RZ-MacBook-Pro.local>
2017-03-07 23:03:02 -0600

    Live Data from API
    Issue with App Token giving 403 error, have to check that to increase
    amount of data

--- opendatademo/wrangle.py
+++ opendatademo/wrangle.py
@@ -1,5 +1,7 @@
 import csv
 import re
+import json
+import requests
 from datetime import datetime
 import folium
 
@@ -7,54 +9,61 @@ import folium
 # And run `pip install -e` from this repo folder.
 # The repo in standard `pip install ...` db does not work with python 3.
 # If you are running Python 2, just run `pip install pyzipcode`.
+
+APP_TOKEN = 'HnqXaIwPkD3s0YQQBY4w0Rcez'
+#url = "https://data.austintexas.gov/resource/5h38-fd8d.json?$$app_token=APP_TOKEN"
+url = "https://data.austintexas.gov/resource/5h38-fd8d.json"
+
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json() 
+else:
+	print response.status_code
+
 from pyzipcode import ZipCodeDatabase
 zcdb = ZipCodeDatabase()
 
 map_zipAvg = folium.Map(location=[30.288009, -97.739133]) # location in center of UT
 
-with open('311data.csv') as inf:
-	data = csv.DictReader(inf)
+# schema: {zip: average time}
+avg_zipcode_times = {}
+for incident in data:
+	lat = lon = None	
+	try:
+		zipc = incident.get("sr_location_zip_code", "Null")
+		if not zipc:
+			continue
+		else:
+			zipc = int(zipc)
+		
+		# extract date_changed
+		# TODO: demo looking at python datetime module
+		# https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
+		date_changed = datetime.strptime(incident.get("sr_updated_date"), \
+										'%Y-%m-%dT%H:%M:%S.%f')
 
-	# schema: {zip: average time}
-	avg_zipcode_times = {}
-	for row in data:
-		lat = lon = None	
-		try:
-			zipc = row['Zip Code']
-			if not zipc:
-				continue
-			else:
-				zipc = int(zipc)
-			
-			# extract date_changed
-			# TODO: demo looking at python datetime module
-			# https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
-			temp = row['Status Change Date']
-			date_changed = datetime.strptime(row['Status Change Date'], \
-											'%m/%d/%Y %I:%M:%S %p')
+		# extract date_created
+		date_created = datetime.strptime(incident.get("sr_created_date"), \
+										'%Y-%m-%dT%H:%M:%S.%f')
 
-			# extract date_created
-			date_created = datetime.strptime(row['Created Date'], \
-											'%m/%d/%Y %I:%M:%S %p')
+		# TODO: lookup datetime subtract
+		# https://docs.python.org/3/library/datetime.html#timedelta-objects
+		# Sanity check for total_seconds() method
+		response_time = (date_changed - date_created).total_seconds()
 
-			# TODO: lookup datetime subtract
-			# https://docs.python.org/3/library/datetime.html#timedelta-objects
-			# Sanity check for total_seconds() method
-			response_time = (date_changed - date_created).total_seconds()
+		if zipc in avg_zipcode_times:
+			avg_zipcode_times[zipc] = (avg_zipcode_times[zipc] + response_time) / 2
+		else:
+			avg_zipcode_times[zipc] = response_time
 
-			if zipc in avg_zipcode_times:
-				avg_zipcode_times[zipc] = (avg_zipcode_times[zipc] + response_time) / 2
-			else:
-				avg_zipcode_times[zipc] = response_time
+	except Exception as e:
+		print(e)
 
-		except Exception as e:
-			print(e)
-
-	print('Creating map... showing avg per zip code')
-	for zipc, avg in avg_zipcode_times.items():
-		latlong = zcdb[zipc]
-		folium.Marker([latlong.latitude, latlong.longitude], popup=str(avg)).add_to(map_zipAvg)
-		print(zipc, avg)
+print('Creating map... showing avg per zip code')
+for zipc, avg in avg_zipcode_times.items():
+	latlong = zcdb[zipc]
+	folium.Marker([latlong.latitude, latlong.longitude], popup=str(avg)).add_to(map_zipAvg)
+	print(zipc, avg)
 
 print('Saving to html map...')
 map_zipAvg.save('zipPlot.html')
