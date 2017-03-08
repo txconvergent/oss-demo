d87f2a8abb90b402e41429bf7635558a6f48c541
Hassaan Raza <RAZA@RZ-MacBook-Pro.local>
2017-03-08 03:40:54 -0600

    Updated URL and added incident count
    So the csv file has 3000 entries, the JSON file has 1000, but they’re
    updated every 30 minutes

--- opendatademo/wrangle.py
+++ opendatademo/wrangle.py
@@ -11,8 +11,8 @@ import folium
 # If you are running Python 2, just run `pip install pyzipcode`.
 
 APP_TOKEN = 'HnqXaIwPkD3s0YQQBY4w0Rcez'
-#url = "https://data.austintexas.gov/resource/5h38-fd8d.json?$$app_token=APP_TOKEN"
-url = "https://data.austintexas.gov/resource/5h38-fd8d.json"
+url = "https://data.austintexas.gov/resource/5h38-fd8d.json?$$app_token=%s" % APP_TOKEN
+#url = "https://data.austintexas.gov/resource/5h38-fd8d.json"
 
 response = requests.get(url)
 if response.status_code == 200:
@@ -26,8 +26,10 @@ zcdb = ZipCodeDatabase()
 map_zipAvg = folium.Map(location=[30.288009, -97.739133]) # location in center of UT
 
 # schema: {zip: average time}
+incident_count = 0
 avg_zipcode_times = {}
 for incident in data:
+	incident_count =  incident_count + 1
 	lat = lon = None	
 	try:
 		zipc = incident.get("sr_location_zip_code", "Null")
@@ -39,7 +41,7 @@ for incident in data:
 		# extract date_changed
 		# TODO: demo looking at python datetime module
 		# https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
-		date_changed = datetime.strptime(incident.get("sr_updated_date"), \
+		date_changed = datetime.strptime(incident.get("sr_status_date"), \
 										'%Y-%m-%dT%H:%M:%S.%f')
 
 		# extract date_created
@@ -58,7 +60,7 @@ for incident in data:
 
 	except Exception as e:
 		print(e)
-
+print('Number of entries:%d' % incident_count)
 print('Creating map... showing avg per zip code')
 for zipc, avg in avg_zipcode_times.items():
 	latlong = zcdb[zipc]
